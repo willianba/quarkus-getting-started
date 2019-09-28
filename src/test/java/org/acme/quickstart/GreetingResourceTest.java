6827a9f6757e4593de9d6c817c547b0a386c7942
Willian Alves <wba.alves@gmail.com>
2019-09-28 13:55:20 -0300

    create unit test for greeting service

--- src/test/java/org/acme/quickstart/GreetingResourceTest.java
+++ src/test/java/org/acme/quickstart/GreetingResourceTest.java
@@ -6,16 +6,30 @@ import org.junit.jupiter.api.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.CoreMatchers.is;
 
+import java.util.UUID;
+
 @QuarkusTest
 public class GreetingResourceTest {
 
     @Test
     public void testHelloEndpoint() {
         given()
-          .when().get("/hello")
-          .then()
-             .statusCode(200)
-             .body(is("hello"));
+        .when()
+            .get("/hello")
+        .then()
+            .statusCode(200)
+            .body(is("hello"));
     }
 
+    @Test
+    public void testGreetingEndpoint() {
+        String uuid = UUID.randomUUID().toString();
+        given()
+            .pathParam("name", uuid)
+        .when()
+            .get("/hello/greeting/{name}")
+        .then()
+            .statusCode(200)
+            .body(is("hello " + uuid));
+    }
 }
